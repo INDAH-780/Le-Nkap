f4b31c3dded434bedde4329e5a4a74148112bf4c
INDAH-780 <mbahindah780@gmail.com>
2024-01-20 16:08:38 +0100

    newly updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "reactreact-router-dom";
 
 function SignUp() {
     const [Name, setName] = useState("");
